7d8c9c2340b8a23e74ee3152d518f21db3c34b6f
justkaneri@yandex.ru <justkaneri@yandex.ru>
2020-10-09 19:27:55 +0300

    Фигура №3

---
+++ Тетрис/Model/Figura_2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Тетрис.Model
+{
+    class Figura_2:MainFigura
+    {
+        /*     /\
+              /  \
+              ----
+        */
+        public Figura_2()
+        {
+            MaxPosition = 4;
+            ArrayStatus = new int[4][,]
+            {
+            new int[,] { {0,3,0},
+                         {3,3,3} },
+
+            new int[,] { {3,0}, 
+                         {3,3}, 
+                         {3,0} },
+
+            new int[,] { {3,3,3},
+                         {0,3,0}},
+
+            new int[,] { {0,3}, 
+                         {3,3},
+                         {0,3} }
+            };
+
+            CurrentPosition = rnd.Next(MaxPosition);
+            SelectStatus();
+
+        }
+    }
+}
